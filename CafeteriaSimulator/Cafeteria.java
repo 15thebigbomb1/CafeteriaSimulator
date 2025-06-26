8117a71a41bde5310eeb63c3d5bb1a803d784892
15thebigbomb1 <166461579+15thebigbomb1@users.noreply.github.com>
2025-06-27 10:39:54 +1200

    Add files via upload
    I have calculated average student and teacher wait time my next step is gui,.

--- CafeteriaSimulator/Cafeteria.java
+++ CafeteriaSimulator/Cafeteria.java
@@ -30,25 +30,24 @@ public class Cafeteria
 
     public void defineCSVFile() {
         File  thefile = new File(filename);  // generate the file handle
-        String CSVlines[] = new String[MAXLINES];
+        String CSVlines[] = new String[MAXLINES]; //holds the csvLine
         String AllLinesAllElements[][]=new String[MAXLINES][VALUESPERLINE];  // where we keep all those lines we read in.
         int linecount=0;  // initially keeps track of lines read, eventually used to remember the number that was read;
 
         try {
             Scanner reader = new Scanner(thefile); // open the file with the Scanner
 
-            // Read in the file, stop at file end or if we read too many lines
             while (reader.hasNextLine()  && linecount < MAXLINES){
                 String line=reader.nextLine();             
                 CSVlines[linecount]=line;
                 linecount++;
             }
-
-            // Now we have all the lines, lets print them out enmass
-            // This is just to prove we have read them in okay.  In reality, we don't need to do this step.
+            // reads the file and adds it to the csvLines array 
             for (int i =1; i<linecount; i++){
                 System.out.println(CSVlines[i]);
                 values = CSVlines[i].split(",");
+                //Takes the csv line text and splits them into a unique string every time there-
+                //is a comma (,)
                 for (int j=0;j<values.length;j++) {
                     switch (j) {
                         case 0:
@@ -67,6 +66,8 @@ public class Cafeteria
                             System.out.println("not regonised");
                             break;
                     }
+                    // switch statement that takes the 3 variables from the csv file-
+                    //-Student,Staff,Served and adds them each to diffrent arrays for each type.
                 }
             }
 
@@ -78,52 +79,70 @@ public class Cafeteria
                 System.out.println("Amount served "+servedAmount.get(i));
                 System.out.println();
             }
+            //prints out the amounts of student,staff,served in each row.
+            
         } catch (IOException e) {System.out.println(e);}
     }
 
     public void RunCafetria() {
         System.out.println("How long do you want to run the simulator for?");
         int timeFrame = (kb.nextInt() -1);
+        // lets the user choose the amount of times the for loop runs and therefore-
+        //-how many minutes the simulation wll go for
         int whileLoopValue = 0;
+        //defining the whileLoopValue 
         Elements head = queue.head;
+        //defines the head element found in the queue class
         Elements tail = queue.Tail;
+        //defines the tail element found in the queue class
+        
         for (int i = 0;i<=timeFrame;i++) {
             int studentValue = Integer.parseInt(studentAmount.get(i));
+            //Amount of students in a csv line that will be pushed in the queue
             System.out.println("Student amount is "+studentValue);
 
             int staffValue = Integer.parseInt(staffAmount.get(i));
+            //Amount of teachers in a csv line that will be pushed in the queue
             System.out.println("Staff amount is "+staffValue);
 
             int servedValue = Integer.parseInt(servedAmount.get(i));
+            //Amount of students and teachers in the csv line that will be served in the queue
             System.out.println("Served amount is "+servedValue);
-
-            System.out.println();
 
             while (whileLoopValue < studentValue) {
                 queue.push(0,1);
                 whileLoopValue++;
             }
             whileLoopValue = 0;
+            //pushes the student objects
+            
             while (whileLoopValue < staffValue) {
                 queue.push(0,2);
                 whileLoopValue++;
             }
             whileLoopValue = 0;
+            //pushes the teacher objects
+            
             while (whileLoopValue < servedValue) {
                 queue.pop();
                 whileLoopValue++;
             }
             whileLoopValue = 0;
-            queue.updateTime();
+            //amount of times they serve the students and teacher
             
+            
+            queue.updateTime();
+            //runs code in the queue class that will add one minutes to every object in the queue 
             head = queue.head;
+            //redfining the head of the queue as it has changed from all the values we have pushed
             Elements next = head;
-            System.out.println("head is "+next.getValue());
-            System.out.println();
+            //System.out.println("head is "+next.getValue());
+            
             float divideStudent = 0;
             float divideStaff = 0;
             float totalStudent = 0;
             float totalStaff = 0;
+            //values for finding the averages 
             while (next != null) {
                 if (next.getType() == "Teacher") {
                     totalStaff = totalStaff + next.getValue();
@@ -132,18 +151,26 @@ public class Cafeteria
                     totalStudent = totalStudent + next.getValue();
                     divideStudent++;
                 }
+                //will add to the amounts of teachers and students 
                 next = next.nextStack();
+                //goes to the next object in the queue 
             }
+            //while loop that gets all the time values for every object in the queue
             
             System.out.println("Divide by for student is "+divideStudent+" and for teacher is "+divideStaff);
             System.out.println("Total for student is "+totalStudent+" and for teachers is "+totalStaff);
             float staffAverage = totalStaff/divideStaff;
+            //staff average time
             float studentAverage = totalStudent/divideStudent;
+            //student average time
             float average = (totalStaff + totalStudent)/(divideStudent + divideStaff);
+            //average time for both students and teachers
+            
             System.out.println("The average wait time for "+(i+1)+" minutes for students is "+studentAverage);
             System.out.println("The average wait time for "+(i+1)+" minutes for staff is "+staffAverage);
             System.out.println("The avaerage wait time for both students and teachers is "+average);
+            System.out.println();
+            System.out.println();
         }
-
     }
 }
--- CafeteriaSimulator/Queue.java
+++ CafeteriaSimulator/Queue.java
@@ -9,7 +9,7 @@ public class Queue
 {
     public Elements head;
     public Elements Tail;
-    
+    //the head and tail of the queue, i.e the front and back
     public Queue() {
         
     }
@@ -20,38 +20,47 @@ public class Queue
     // returns if queue is empty
     public void push (int value, int valueTwo) {
         Elements element = new Elements();
+        //creates new element
         element.setValue(value);
+        //defines the value of the object aka the int amount
         if (valueTwo == 1) {
             element.setStudent();
         } else {
             element.setTeacher();
         }
-         
+        // defines if the object is a teacher or a student
         if (queueEmpty()) {
             head = element;
         }
-        
+        //if the queue is empty then the next object will have to be the head of the object
         if (Tail != null) {
             Tail.LinkTo(element);
         }
+        //if the tail is defined then the tail gets linked to the element
         Tail = element;
+        //element is defined as tail
         
         
     }
-    // creates 
+    // creates/pushes a new object into the queue
     public void updateTime() {
         Elements timeUpdater = head;
+        //makes the first element updated the front of the queue
         while (timeUpdater != null) {
            timeUpdater.setValue((timeUpdater.getValue()+1));
            
            timeUpdater = timeUpdater.nextStack();
+           //defines the timeUpdater element as the next element in queue 
         }
+        //while loop that adds 1 minute to every object in queue
     }
     
     public int pop() {
         int value=head.getValue();
         head =head.nextStack();
+        //gets the head removed and defines the head as the next element in stack
         return value;
+        //returns the previous head value
     }
-    
+    //removes the element at the front of the queue and defines the one next in queueas the head
 }
