fb9f97c1daf8a730ad5f6960b934089e0c01fdf9
15thebigbomb1 <166461579+15thebigbomb1@users.noreply.github.com>
2025-07-24 12:33:11 +1200

    Add files via upload
    I have added text and a switch atatement so it draws something diffrently when when a diffeent panel is chosen

--- CafeteriaSimulator/Gui.java
+++ CafeteriaSimulator/Gui.java
@@ -19,7 +19,8 @@ public class Gui extends JFrame implements ActionListener,MouseListener
     //Menu item found in the menu.
     JPanel startingPanel;
     JPanel mainPanel;
-    
+    String[] Panels = {"startingPanel","mainPanel","optionPanel","resultPanel"};
+
     //Menu Panel that holds the background colour and canvas.
     Canvas startingGraphic;
     Canvas mainGraphic;
@@ -38,47 +39,47 @@ public class Gui extends JFrame implements ActionListener,MouseListener
 
     Toolkit toolkit = Toolkit.getDefaultToolkit();
     Dimension screenSize = toolkit.getScreenSize();
+    //Tools for finding screen dimensions
     int screenWidth = screenSize.width;
     int screenHeight = screenSize.height;
+    //Screen size
+    int chooseMenuValues = 1;
 
     public Gui() {
         setTitle("Cafeteria Simulator");
         menuBar = new JMenuBar();
-        //Defines menubar in constructer.
+        //Defines menubar in method.
         menuBar.setBackground(Color.GRAY);
-        //Changes the color=ur of the menubar to gray.
+        //Changes the colour of the menubar to gray.
         menuBar.setBorder(BorderFactory.createEmptyBorder());
         //Removes a white border line that is there by default between the bar and canvas to make the windows-
         //-colours blend more nicely.
-
         this.setJMenuBar(menuBar);  
         //adds menubar to default panel.
         menu = new JMenu("Options");
         // Defines the option menu in constucter
         menu.setForeground(Color.WHITE);
         //Defines the colour of the menus text.
-        menuBar.add(menu);
-        // adds the menu to the menuBar.
         menuItem = new JMenuItem("Exit");
-        //defines menuItem in constructer.
+        //defines menuItem in method.
         menuItem.addActionListener(this);
         //Adds a listener so when its pressed it'll run a method.
+        menuBar.add(menu);
         menu.add(menuItem);
-        //Adds the menuitem to the menu.
+        //Adds the menu to the menubar and adds the menuitem to the menu.
+        
         startingPanel = new JPanel();
         mainPanel = new JPanel();
-        //Defines startingPanel in constructer.
+        //Defines the panels that will be used in the gui.
         startingPanel.setBackground(panelColor);
         mainPanel.setBackground(Color.RED);
-        
         //Changes the colour to the color chosen for the background.
         startingGraphic = new Canvas();
         mainGraphic = new Canvas();
-        //Defines the canvas in the constructer.
+        //Defines the canvas that will be used in the gui.
         startingPanel.add(startingGraphic); 
         mainPanel.add(mainGraphic);
-        //Adds the canvas to the startingPanel.
-        
+        //Adds the canvases to the panels.
 
         this.getContentPane().add(startingPanel);
         // Reupdates the startingPanel to update the colour of the background.
@@ -99,23 +100,27 @@ public class Gui extends JFrame implements ActionListener,MouseListener
         addMouseListener(this);
         //putting the window to the fronting and defining its viasibility to true
     }
-    public void mainGraphic() {
-        this.getContentPane().remove(startingPanel);
-        this.getContentPane().add(mainPanel);
-        this.pack();
-        this.toFront();
-        
-    }
+
+    
+
     public void ChooseMenu(int chooseMenuValue) {
         switch (chooseMenuValue) {
-        case 1:
-            System.out.println("Starting menu chosen");
-            break;
-        case 2:
-            System.out.println("Second menu chosen");
-            mainGraphic();
-        case 3:
-            System.out.println("Option menu chosen:");
+            case 1:
+                System.out.println("Starting menu chosen");
+                repaint();
+                chooseMenuValues = 1;
+                break;
+            case 2:
+                System.out.println("Second menu chosen");
+                mainPanel();
+                chooseMenuValues = 2;
+                repaint();
+                break;
+            case 3:
+                System.out.println("Option menu chosen:");
+                chooseMenuValues = 3;
+                repaint();
+                break;
         }
     }
 
@@ -130,42 +135,62 @@ public class Gui extends JFrame implements ActionListener,MouseListener
                 break;
         }
     }
- 
+
     public void paint (Graphics g ) {
         super.paint(g);
         //Defines the painting method
-        whsLogo.paintIcon(this,g,157,100);
-        //Paints the whs logo with its x and y corrodinates.
         Graphics2D g2 = (Graphics2D) g; 
         //Defines the 2d paint methods for circles/rectangles etc.
-        g2.setFont(new Font("Franklin Gothic Demi",Font.BOLD,35));
-        //Sets font.
-        g2.setColor(new Color(255,238,140));
-        //Sets colour.
-        g2.drawString("Wellington High",97,270);
-        g2.drawString("Cafeteria Simulator",71,300);
-        //Paints the text onto the canvas.
 
-        g2.setColor(Color.WHITE);
-        //Sets colour.
-        g2.fillRect(200,320,65,25);
-        //Paints a colour filled rectangle and defines size and corrodinates.
-        g2.setColor(Color.BLACK);
-        //Sets Colour.
-        g2.setStroke(new BasicStroke(3));
-        //Changes the brush stroke to make it thicker.
-        g2.drawRect(200,320,65,25);
-        //Draws another rectangle over the filled rectangle in a diffrent colour-
-        //-to create a border for it.
-        g2.setFont(new Font("Franklin Gothic Demi",Font.BOLD,25));
-        //Sets font.
-        g2.setColor(Color.BLACK);
-        //Sets colour.
-        g2.drawString("start",203,340);
-        //Draws the text found in the rectancle to make it look like a-
-        //-button.
+        switch (chooseMenuValues) {
+
+            case 1:
+                whsLogo.paintIcon(this,g,157,100);
+                //Paints the whs logo with its x and y corrodinates.
+                g2.setFont(new Font("Franklin Gothic Demi",Font.BOLD,35));
+                //Sets font.
+                g2.setColor(new Color(255,238,140));
+                //Sets colour.
+                g2.drawString("Wellington High",97,270);
+                g2.drawString("Cafeteria Simulator",71,300);
+                //Paints the text onto the canvas.
+                g2.setColor(Color.WHITE);
+                //Sets colour.
+                g2.fillRect(200,320,65,25);
+                //Paints a colour filled rectangle and defines size and corrodinates.
+                g2.setColor(Color.BLACK);
+                //Sets Colour.
+                g2.setStroke(new BasicStroke(3));
+                g2.drawRect(200,320,65,25);
+                //Draws another rectangle over the filled rectangle in a diffrent colour-
+                //-to create a border for it.
+                g2.setFont(new Font("Franklin Gothic Demi",Font.BOLD,25));
+                //Sets font.
+                g2.setColor(Color.BLACK);
+                //Sets colour.
+                g2.drawString("start",203,340);
+                //Draws the text found in the rectancle to make it look like a-
+                //-button.
+                break;
+            case 2:
+                g2.setColor(Color.GREEN);
+                g2.setFont(new Font("Franklin Gothic Demi",Font.BOLD,35));
+                g2.drawString("Hello World!",97,270);
+                break;
+        }
     }
-
+    public void startingPanel() {
+        this.getContentPane().add(startingPanel);
+        
+    }
+    
+    public void mainPanel() {
+        this.getContentPane().remove(startingPanel);
+        this.getContentPane().add(mainPanel);
+        this.pack();
+        this.toFront();
+    }
+    
     public void mouseExited(MouseEvent e) {};
 
     public void mouseEntered(MouseEvent e) {};
@@ -175,11 +200,17 @@ public class Gui extends JFrame implements ActionListener,MouseListener
         //Defines the mouses x corrodinate.
         int mouseY = e.getY();
         //Defines the mouses Y corrodinate.
-        if (mouseX >= 200 && mouseX <= 265 && mouseY >= 320 && mouseY <= 345) {
-            System.out.println("button Pressed");
-            ChooseMenu(2);
+        switch (chooseMenuValues) {
+            case 1: 
+                if (mouseX >= 200 && mouseX <= 265 && mouseY >= 320 && mouseY <= 345) {
+                    System.out.println("button Pressed");
+                    ChooseMenu(2);
+                }
+                break;
+            case 2:
+                System.out.println("Menu Two!!!");
+                break;
         }
-        //Defines the menu 
     }
 
     public void mousePressed(MouseEvent e) {};
